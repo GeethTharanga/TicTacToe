2465623717e1eb716c4757878e9a854bd00cd043
Geeth Tharanga <getthh@gmail.com>
2015-05-06 16:30:08 +0530

    initial. added board and gamestate

---
+++ TicTacToe/Core/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Core
+{
+    public enum CellType
+    {
+        Clear, Player1, Player2
+    }
+    public enum Status
+    {
+        NotStarted, TurnP1, TurnP2, WonP1, WonP2, Cancelled
+    }
+    public class Board
+    {
+        private CellType[,] cells;
+        public Status  CurrentStatus { get; private set; }
+
+        public CellType this[int row,int col]
+        {
+            get { return CellType.Clear; }
+            set { }
+        }
+    }
+}
