b309efeb17920cc370240993ce910faf5a4f331a
Geeth Tharanga <getthh@gmail.com>
2015-05-07 12:07:00 +0530

    Finished board and its tests

--- T3Test/Core/BoardTests.cs
+++ T3Test/Core/BoardTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe.Core;
 
 namespace T3Test.Core
 {
@@ -7,8 +8,124 @@ namespace T3Test.Core
     public class BoardTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestInitialization()
         {
+            Board b = new Board();
+            Assert.AreEqual(Status.NotStarted,b.CurrentStatus);
+
+            Assert.AreEqual( CellType.Clear,b[0, 0]);
+            Assert.AreEqual(CellType.Clear,b[1, 2]);
+
+            b.StartGame(Player.Player1);
+            Assert.AreEqual(Status.TurnP1,b.CurrentStatus);
+
         }
+
+        [TestMethod]
+        public void TestAsigning()
+        {
+            Board b = new Board();
+            b.StartGame(Player.Player1);
+            
+            b[1, 1] = CellType.Player1;
+
+            Assert.AreEqual( CellType.Player1,b[1, 1]);
+            Assert.AreEqual(Status.TurnP2,b.CurrentStatus);
+            Assert.AreEqual(1,b.NumCheckedCells);
+
+            Assert.IsTrue(b.IsGameInProgress);
+            Assert.IsFalse(b.IsGameEnded);
+
+            try
+            {
+                b[1, 1] = CellType.Player2;
+                Assert.Fail("No exception - overwrite");
+            }
+            catch (InvalidOperationException )
+            {      }
+
+            try
+            {
+                b[3, 1] = CellType.Player2;
+                Assert.Fail("No exception - out of range");
+            }
+            catch (ArgumentOutOfRangeException )
+            {}
+
+            try
+            {
+                b[2, 1] = CellType.Player1;
+                Assert.Fail("No exception - wrong player");
+            }
+            catch (InvalidOperationException)
+            { }
+
+            //cancelling
+            b.CancelGame();
+            Assert.IsTrue(b.IsGameEnded);
+            Assert.IsFalse(b.IsGameInProgress);
+            Assert.AreEqual(Status.Cancelled, b.CurrentStatus);
+            try
+            {
+                b[0, 2] = CellType.Player2;
+                Assert.Fail("No exception - cancelled game");
+            }
+            catch (InvalidOperationException)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void TestWinning()
+        {
+            Board b = new Board();
+            b.StartGame(Player.Player2);
+
+            Assert.IsTrue(b.IsGameInProgress);
+            b[1, 1] = CellType.Player2;
+            b[1, 2] = CellType.Player1;
+            Assert.IsTrue(b.IsGameInProgress);
+            b[0, 0] = CellType.Player2;
+            b[0, 2] = CellType.Player1;
+            Assert.IsTrue(b.IsGameInProgress);
+            Assert.IsFalse(b.IsGameEnded);
+            b[2, 2] = CellType.Player2;
+
+            Assert.IsFalse(b.IsGameInProgress);
+            Assert.IsTrue(b.IsGameEnded);
+            Assert.AreEqual(Status.WonP2, b.CurrentStatus);
+
+            try
+            {
+                b[2, 0] = CellType.Player1;
+                Assert.Fail("No exception- doing moves after winning");
+            }
+            catch (InvalidOperationException)
+            { }
+        }
+
+        [TestMethod]
+        public void TestTie()
+        {
+            Board b = new Board();
+            b.StartGame(Player.Player2);
+
+            b[0, 0] = CellType.Player2;
+            b[0, 1] = CellType.Player1;
+            b[0, 2] = CellType.Player2;
+            b[1, 1] = CellType.Player1;
+            b[1, 0] = CellType.Player2;
+            b[2, 0] = CellType.Player1;
+            b[1, 2] = CellType.Player2;
+            b[2, 2] = CellType.Player1;
+
+            Assert.IsTrue(b.IsGameInProgress);
+            b[2, 1] = CellType.Player2;
+            Assert.IsTrue(b.IsGameEnded);
+            Assert.IsFalse(b.IsGameInProgress);
+            Assert.AreEqual(Status.Tie, b.CurrentStatus);
+            
+        }
+
     }
 }
--- TicTacToe/Core/Board.cs
+++ TicTacToe/Core/Board.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,24 +9,160 @@ namespace TicTacToe.Core
 {
     public enum CellType
     {
-        Clear, Player1, Player2
+        Clear=0, Player1, Player2
     }
     public enum Status
     {
-        NotStarted, TurnP1, TurnP2, WonP1, WonP2, Cancelled
+        NotStarted, TurnP1, TurnP2, WonP1, WonP2,Tie, Cancelled
     }
+    public enum Player
+    {
+        Player1,Player2
+    }
+
     public class Board
     {
+        //logger
+        Logger logger = LogManager.GetCurrentClassLogger();
+        //attributes
         private CellType[,] cells;
         public Status  CurrentStatus { get; private set; }
-
-        public CellType this[int row,int col]
+        
+        //ctor
+        public Board()
         {
-            get { return CellType.Clear; }
-            set { }
+            logger.Info("Creating new board");
+            
+            cells = new CellType[3,3];
+            CurrentStatus = Status.NotStarted;
         }
 
-        public void StartGame() {  }
-        public void CancelGame(){}
+        //cell management
+        public CellType this[int row,int col]
+        {
+            get
+            {
+                if (row < 0 || col < 0 || row >= 3 || col >= 3)
+                {
+                    throw new ArgumentOutOfRangeException("Invalid Cell");
+                }
+                else
+                {
+                    return cells[row, col];
+                }
+            }
+            set
+            {
+                logger.Info("Trying to set cell {0},{1} to {2}", row, col, value);
+                if (row < 0 || col < 0 || row >= 3 || col >= 3)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else if (cells[row, col] != CellType.Clear)
+                {
+                    throw new InvalidOperationException("Tried to overwrite cell");
+                }
+                else if (!IsGameInProgress)
+                {
+                    throw new InvalidOperationException("Game not in playing state");
+                }
+                else if (!( (value == CellType.Player1 && Status.TurnP1 == CurrentStatus) ||
+                    (value == CellType.Player2 && Status.TurnP2 == CurrentStatus)) )
+                {
+                    throw new InvalidOperationException("Invalid player");
+                }
+                else
+                {
+                    cells[row, col] = value;
+                    CurrentStatus = (CurrentStatus == Status.TurnP1) ? Status.TurnP2 : Status.TurnP1;
+                    UpdateTableState();
+                }
+            }
+        }
+
+        public void StartGame(Player startPlayer) 
+        {
+            
+            if(CurrentStatus == Status.NotStarted )
+            {
+                logger.Info("Starting game");
+                CurrentStatus = startPlayer == Player.Player1 ? Status.TurnP1 : Status.TurnP2;
+            }
+            else
+            {
+                throw new InvalidOperationException("Game already started");
+            }
+        }
+        public void CancelGame()
+        {
+            CurrentStatus = Status.Cancelled;
+        }
+
+        public int NumCheckedCells
+        {
+            get
+            {
+                return cells.OfType<CellType>().Count(c=> c!= CellType.Clear);
+            }
+        }
+
+        public bool IsGameInProgress
+        {
+            get
+            {
+                return CurrentStatus == Status.TurnP1 || CurrentStatus == Status.TurnP2;
+            }
+        }
+
+        public bool IsGameEnded
+        {
+            get
+            {
+                var endStates=new[] { Status.WonP1, Status.WonP2,Status.Tie, Status.Cancelled };
+                return endStates.Contains(CurrentStatus);
+            }
+        }
+
+        /// <summary>
+        /// Update game status according to last move
+        /// To check if someone won or game tied.
+        /// </summary>
+        private void UpdateTableState()
+        {
+            if(!IsGameInProgress)
+            {
+                throw new InvalidOperationException("This method shouldn't be called when game is not playing");
+            }
+
+            //for each possible winning combination - line
+            foreach (int[,] combination in WinningCombinations.Combinations)
+            {
+                //find current celltypes in this line
+                List<CellType> line = new List<CellType>(3);
+                for (int i = 0; i < 3; i++) 
+                {
+                    int cellRow = combination[i, 0], cellCol = combination[i, 1];
+                    CellType cell = cells[cellRow, cellCol];
+                    line.Add(cell);
+                }
+
+                //declare winner if all cells in the line belongs to same player
+                if(line.TrueForAll(c => c== CellType.Player1 ) )
+                {
+                    CurrentStatus = Status.WonP1;
+                }
+                else if (line.TrueForAll(c=> c==CellType.Player2) )
+                {
+                    CurrentStatus = Status.WonP2;
+                }
+
+                //mark as a tie if still in progress(not won), and all cells are marked
+                if(IsGameInProgress && NumCheckedCells == 9)
+                {
+                    CurrentStatus = Status.Tie;
+                }
+            }
+        }
+
     }
 }
