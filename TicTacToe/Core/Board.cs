b599bddbcf01050e2b76de4a5007791cd86fee72
Geeth Tharanga <getthh@gmail.com>
2015-05-07 14:09:51 +0530

    Added more logging

--- TicTacToe/Core/Board.cs
+++ TicTacToe/Core/Board.cs
@@ -27,6 +27,7 @@ namespace TicTacToe.Core
         //attributes
         private CellType[,] cells;
         public Status  CurrentStatus { get; private set; }
+        public int[,] WonConfiguration { get; private set; }
         
         //ctor
         public Board()
@@ -35,6 +36,7 @@ namespace TicTacToe.Core
             
             cells = new CellType[3,3];
             CurrentStatus = Status.NotStarted;
+            WonConfiguration = null;
         }
 
         //cell management
@@ -44,6 +46,7 @@ namespace TicTacToe.Core
             {
                 if (row < 0 || col < 0 || row >= 3 || col >= 3)
                 {
+                    logger.Error("Attempted to access invalid cell {0},{1}", row, col);
                     throw new ArgumentOutOfRangeException("Invalid Cell");
                 }
                 else
@@ -56,19 +59,23 @@ namespace TicTacToe.Core
                 logger.Info("Trying to set cell {0},{1} to {2}", row, col, value);
                 if (row < 0 || col < 0 || row >= 3 || col >= 3)
                 {
+                    logger.Error("Invalid cell {0},{1}", row, col);
                     throw new ArgumentOutOfRangeException();
                 }
                 else if (cells[row, col] != CellType.Clear)
                 {
+                    logger.Error("Attempted to overwrite cell. {0} => {1}", cells[row, col], value);
                     throw new InvalidOperationException("Tried to overwrite cell");
                 }
                 else if (!IsGameInProgress)
                 {
+                    logger.Error("Attempted to change cell in state: {0}", CurrentStatus);
                     throw new InvalidOperationException("Game not in playing state");
                 }
                 else if (!( (value == CellType.Player1 && Status.TurnP1 == CurrentStatus) ||
                     (value == CellType.Player2 && Status.TurnP2 == CurrentStatus)) )
                 {
+                    logger.Error("Wrong cell type. state:{0}. type: {1}", CurrentStatus, value);
                     throw new InvalidOperationException("Invalid player");
                 }
                 else
@@ -90,11 +97,13 @@ namespace TicTacToe.Core
             }
             else
             {
+                logger.Error("Tried to start game again");
                 throw new InvalidOperationException("Game already started");
             }
         }
         public void CancelGame()
         {
+            logger.Info("Game cancelled");
             CurrentStatus = Status.Cancelled;
         }
 
@@ -147,18 +156,25 @@ namespace TicTacToe.Core
                 }
 
                 //declare winner if all cells in the line belongs to same player
+                
+                Lazy<string> combString = new Lazy<string>(() => string.Format("{0},{1}; {2},{3}; {4},{5}",combination.OfType<object>().ToArray()));
                 if(line.TrueForAll(c => c== CellType.Player1 ) )
                 {
                     CurrentStatus = Status.WonP1;
+                    WonConfiguration = combination;
+                    logger.Info("P1 won with {0}", combString.Value);
                 }
                 else if (line.TrueForAll(c=> c==CellType.Player2) )
                 {
                     CurrentStatus = Status.WonP2;
+                    WonConfiguration = combination;
+                    logger.Info("P2 won with {0}", combString.Value);
                 }
 
                 //mark as a tie if still in progress(not won), and all cells are marked
                 if(IsGameInProgress && NumCheckedCells == 9)
                 {
+                    logger.Info("Game tied");
                     CurrentStatus = Status.Tie;
                 }
             }
