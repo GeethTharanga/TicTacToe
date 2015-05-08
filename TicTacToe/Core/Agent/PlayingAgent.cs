e254a0993258607728e78e7be0f0a10552797d3f
Geeth Tharanga <getthh@gmail.com>
2015-05-08 12:01:50 +0530

    added missing files

---
+++ TicTacToe/Core/Agent/PlayingAgent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Core.Agent
+{
+    public class TTTMoveEventArgs
+    {
+        public readonly int row, col;
+        public readonly Player player;
+        public TTTMoveEventArgs(int row, int col, Player player)
+        {
+            this.row = row;
+            this.col = col;
+            this.player = player;
+        }
+    }
+
+    public delegate void TTTMoveEventHandler(object sender, TTTMoveEventArgs e);
+
+    public abstract class PlayingAgent
+    {
+        public string AgentName { get; protected set; }
+
+        protected Player player;
+
+        public abstract async void InformStart(bool firstMove);
+        public abstract async void InformMove(int row, int col, CellType move);
+        public abstract async void InformCancel();
+
+        public event TTTMoveEventHandler MakeMove;
+        public event EventHandler CancelGame;
+        
+        
+        public PlayingAgent(Player thisPlayer)
+        {
+            player = thisPlayer;
+        }
+    }
+}
---
+++ TicTacToe/Core/WinningCombinations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Core
+{
+    public static class WinningCombinations
+    {
+        public static IEnumerable<int[,]> Combinations
+        {
+            get
+            {
+                var comb = new List<int[,]>(8);
+
+                //diagonals
+                comb.Add(new int[3, 2] { { 0, 0 }, { 1, 1 }, { 2, 2 } });
+                comb.Add(new int[3, 2] { { 0, 2 }, { 1, 1 }, { 2, 0 } });
+
+                //horizontal & vertical
+                for (int i = 0; i < 3; i++)
+                {
+                    //horizontal
+                    comb.Add(new int[3, 2] { { i, 0 }, { i, 1 }, { i, 2 } });
+                    //vertical
+                    comb.Add(new int[3, 2] { { 0, i }, { 1, i }, { 2, i } });
+                }
+                return comb;
+            }
+        }
+    }
+}
