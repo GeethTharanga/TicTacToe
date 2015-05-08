b47a3d36fa9d6e1d7fcf73af9ab54c72a7ca3f9b
Geeth Tharanga <getthh@gmail.com>
2015-05-08 17:20:27 +0530

    added ui input base class

---
+++ TicTacToe/UI/UserInputBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Core;
+using TicTacToe.Core.Agent;
+
+namespace TicTacToe.UI
+{
+    public abstract class UserInputBase
+    {
+        protected readonly Board board;
+        protected readonly Player thisPlayer;
+
+        public UserInputBase(Board board,Player player)
+        {
+            this.board = board;
+            this.thisPlayer = player;
+        }
+
+        public abstract void Refresh();
+
+        public event EventHandler OnCancel;
+        public event TTTMoveEventHandler OnMove;
+
+        protected void CancelGame()
+        {
+            OnCancel(this, new EventArgs());
+        }
+        protected void DeclareMove(int row,int col)
+        {
+            var evt = new TTTMoveEventArgs(row, col, thisPlayer);
+            OnMove(this, evt);
+        }
+    }
+}
