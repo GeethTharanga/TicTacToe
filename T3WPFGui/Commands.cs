0e89a4a1e3149bcbf0c97c72957b83aee1ab329a
Geeth Tharanga <getthh@gmail.com>
2015-05-17 21:47:40 +0530

    added game mode selection ui

--- T3WPFGui/Commands.cs
+++ T3WPFGui/Commands.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 namespace T3WPFGui
 {
     public static class Commands
     {
         public static readonly RoutedUICommand MarkCell = new RoutedUICommand("Mark Cell", "MarkCell", typeof(MainWindow));
+
+        public static readonly RoutedUICommand AIGame = new RoutedUICommand("New AI Game", "AIGame", typeof(GameSelectWindow));
+        public static readonly RoutedUICommand HostGame = new RoutedUICommand("Host Game", "HostGame", typeof(GameSelectWindow));
+        public static readonly RoutedUICommand JoinGame = new RoutedUICommand("Join Game", "JoinGame", typeof(GameSelectWindow));
     }
 }
---
+++ T3WPFGui/GameSelectWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TicTacToe.Core;
+using TicTacToe.Core.Agent;
+using TicTacToe.Core.Agent.AI;
+
+namespace T3WPFGui
+{
+    /// <summary>
+    /// Interaction logic for GameSelectWindow.xaml
+    /// </summary>
+    public partial class GameSelectWindow : Window
+    {
+
+
+        public bool NotSelectedOption
+        {
+            get { return (bool)GetValue(NotSelectedOptionProperty); }
+            set { SetValue(NotSelectedOptionProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for NotSelectedOption.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NotSelectedOptionProperty =
+            DependencyProperty.Register("NotSelectedOption", typeof(bool), typeof(GameSelectWindow), new PropertyMetadata(0));
+
+
+        
+        public GameSelectWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public void StartGame(PlayingAgent otherPlayer, Player thisPlayer)
+        {
+            MessageBox.Show("new game" + otherPlayer);
+        }
+
+        private void NewHostGame(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Not implemented");
+        }
+        private void NewAIGame(object sender, ExecutedRoutedEventArgs e)
+        {
+            PlayingAgent ai = new AIRandomAgent(Player.Player2);
+            StartGame(ai, Player.Player1);
+        }
+        private void NewJoinGame(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Not implemented");
+        }
+    }
+}
