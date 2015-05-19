6e0ca3947e0dc9831a1630152a30659564674a57
Geeth Tharanga <getthh@gmail.com>
2015-05-19 21:57:51 +0530

    Remote staring agent

---
+++ TicTacToe/Core/Agent/RemoteStartingAgent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Core.Agent
+{
+    public class RemoteStartArgs  
+    {
+        public bool StartingMove { get; private set; }
+    }
+    public abstract class RemoteStartingAgent : PlayingAgent 
+    {
+        public event EventHandler<RemoteStartArgs> OnRemoteStart;
+
+        protected void DeclareRemoteStart(RemoteStartArgs arg)
+        {
+            if(OnRemoteStart != null )
+            {
+                OnRemoteStart(this, arg);
+            }
+        }
+    }
+}
