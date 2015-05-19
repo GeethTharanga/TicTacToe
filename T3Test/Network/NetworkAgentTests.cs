14e54d4949aa5063be5ae819efc6049a63bce46a
Geeth Tharanga <getthh@gmail.com>
2015-05-19 21:17:34 +0530

    network agent tests

---
+++ T3Test/Network/NetworkAgentTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using T3Network;
+using TicTacToe.Core.Agent.AI;
+using System.Threading;
+using TicTacToe.Core;
+
+namespace T3Test.Network
+{
+    [TestClass]
+    public class NetworkAgentTests
+    {
+        [TestMethod]
+        public void TestNetAgent()
+        {
+            NetworkAgent host, guest;
+            AIRandomAgent hostAi, guestAi;
+
+            NetworkListener listener = new NetworkListener(TicTacToe.Core.Player.Player2);
+            NetworkClient client = new NetworkClient("localhost", TicTacToe.Core.Player.Player1);
+
+            client.OnError += (s, e) => { Console.WriteLine(e); };
+
+            listener.StartListening();
+            client.Connect();
+            Thread.Sleep(100);
+            Assert.IsTrue(client.IsConnected && listener.IsConnected);
+
+            host = listener.Agent;
+            guest = client.Agent;
+
+            hostAi = new AIRandomAgent(TicTacToe.Core.Player.Player1);
+            guestAi = new AIRandomAgent(TicTacToe.Core.Player.Player2);
+
+            GameManager hostManager, guestManager;
+            hostManager = new GameManager(hostAi, host, true);
+            guestManager = new GameManager(guest, guestAi, false);
+
+            hostManager.StartGame();
+            guestManager.StartGame();
+
+            hostAi.OnMove += (s, e) => { Console.WriteLine("hostAI " + e); };
+            host.OnMove += (s, e) => { Console.WriteLine("host " + e); };
+            guestAi.OnMove += (s, e) => { Console.WriteLine("guestAI " + e); };
+            guest.OnMove += (s, e) => { Console.WriteLine("guest " + e); };
+
+            Thread.Sleep(10000);
+        }
+    }
+}
