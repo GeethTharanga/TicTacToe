4b3ef5fb12fe78bcbafceb5ab28daf05b4d08257
Geeth Tharanga <getthh@gmail.com>
2015-06-03 14:33:13 +0530

    added classes to represent a history repo

---
+++ TicTacToe/Stats/GamePlayRecord.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2015 Geeth Tharanga 
+// Under the MIT licence - See licence.txt
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Stats
+{
+    public enum GamePlayOpponent
+    {
+        AIEasy,
+        AIHard,
+        Human
+    }
+    public enum GamePlayResult
+    {
+        Tied,
+        Won,
+        Loss
+    }
+
+    public sealed class GamePlayRecord
+    {
+        public GamePlayOpponent Opponent { get; set; }
+        public DateTime Time { get; set; }
+        public GamePlayResult Result { get; set; }
+    }
+
+    public sealed class GamePlayStatistics
+    {
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+    }
+
+}
---
+++ TicTacToe/Stats/IGameRecordRepository.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2015 Geeth Tharanga 
+// Under the MIT licence - See licence.txt
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Stats
+{
+    interface IGameRecordRepository
+    {
+        void SaveRecord(GamePlayRecord record);
+        IEnumerable<GamePlayRecord> GetLastRecords(int maxCount);
+        IDictionary<GamePlayOpponent, GamePlayStatistics> GetStatistics();
+        void ClearHistory();
+    }
+}
