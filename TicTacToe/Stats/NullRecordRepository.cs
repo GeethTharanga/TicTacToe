460fcc0e42c8f823afa0f66a2ff402efc099a2b1
Geeth Tharanga <getthh@gmail.com>
2015-06-03 14:52:17 +0530

    null record repo

---
+++ TicTacToe/Stats/NullRecordRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Stats
+{
+    public class NullRecordRepository : IGameRecordRepository 
+    {
+        public void SaveRecord(GamePlayRecord record)
+        {
+        }
+
+        public IEnumerable<GamePlayRecord> GetLastRecords(int maxCount)
+        {
+            return new GamePlayRecord[0];
+        }
+
+        public IDictionary<GamePlayOpponent, GamePlayStatistics> GetStatistics()
+        {
+            Dictionary<GamePlayOpponent, GamePlayStatistics> ret = new Dictionary<GamePlayOpponent, GamePlayStatistics>();
+            GamePlayStatistics stat = new GamePlayStatistics { Losses = 0, Wins = 0 };
+            ret[GamePlayOpponent.AIEasy] = stat;
+            ret[GamePlayOpponent.AIHard] = stat;
+            ret[GamePlayOpponent.Human] = stat;
+
+            return ret;
+        }
+
+        public void ClearHistory()
+        {
+        }
+    }
+}
